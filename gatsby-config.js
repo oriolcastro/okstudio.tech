9b68265e36a821726539cc4da65748dd5f22c3cc
Uri <oriol.castroarnau@gmail.com>
2018-06-12 15:11:13 +0200

    Second comit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.MY_READONLY_DATOCMS_API_TOKEN || '',
+        previewMode: true,
+        disableLiveReload: true,
+      },
+    },
+  ],
 }
