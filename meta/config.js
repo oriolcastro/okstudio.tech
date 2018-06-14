08a17d38d5aff7a2bab893eea4076aa308b5cd88
Uri <oriol.castroarnau@gmail.com>
2018-06-14 17:23:54 +0200

    SItemap plugin error
    Solves the error during build time

--- meta/config.js
+++ meta/config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  siteTitle: '', // Site title.
-  siteTitleAlt: '', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: '', // Domain of your website without pathPrefix.
+  siteTitle: 'Ok! studio', // Site title.
+  siteTitleAlt: 'Ok! studio website', // Alternative site title for SEO.
+  siteLogo: '/icons/icon.png', // Logo used for SEO and manifest.
+  siteUrl: 'http://okstudio.tech', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: '', // Website description used for RSS feeds/meta description tag.
   copyright: '', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '', // Used for setting manifest and progress theme colors.
-  backgroundColor: '', // Used for setting manifest background color.
   googleanalytics: '', //Google Analytics tracker
 }
