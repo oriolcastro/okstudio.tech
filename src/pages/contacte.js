2e91ad56579d9bfdfc6c3e70a22c5aa18556cfbc
Uri <oriol.castroarnau@gmail.com>
2018-06-21 12:58:48 +0200

    Contacte page

--- src/pages/contacte.js
+++ src/pages/contacte.js
@@ -1,15 +1,110 @@
 import React from 'react'
-// import { Link } from 'gatsby'
-import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
+import {
+  Section,
+  Container,
+  Field,
+  Label,
+  Control,
+  Input,
+  TextArea,
+  Radio,
+  Button,
+  Title,
+  Columns,
+  Column,
+  Content,
+  Checkbox,
+} from 'bloomer'
+
+import Layout from '../components/layout'
 import PageHero from '../components/pagehero'
 
 const ContactePage = () => (
   <Layout>
     <PageHero
       title="Estem aquí per ajudar-te"
-      subtitle="Contacta'ns i explica'ns quina necessitat tens, segur que trobarem la forma de treballar plegats"
+      subtitle="Si ens expliques quina necessitat tens, segur que trobarem la forma de treballar plegats"
     />
+    <Section>
+      <Container>
+        <Columns isMultiline>
+          <Column isSize={6} isOffset={1}>
+            <form
+              name="contactPage"
+              method="POST"
+              netlify-honeypot="bot-field"
+              netlify
+            >
+              <Field>
+                <Label>Nom i cognoms</Label>
+                <Control>
+                  <Input type="text" name="name" placeholder="Jack Sparrow" />
+                </Control>
+              </Field>
+              <p className="is-hidden">
+                <label>
+                  Don’t fill this out if you're human:{' '}
+                  <input name="bot-field" />
+                </label>
+              </p>
+              <Field>
+                <Label>Correu electrònic</Label>
+                <Control>
+                  <Input type="email" name="email" placeholder="@" />
+                </Control>
+              </Field>
+              <Field>
+                <Label>Com et podem ajudar</Label>
+                <Control>
+                  <TextArea
+                    name="message"
+                    placeholder={'Explica tot allò que creguis necessari'}
+                  />
+                </Control>
+              </Field>
+              <Field>
+                <Control>
+                  <Checkbox> Accepto la política de privacitat </Checkbox>
+                </Control>
+              </Field>
+              <Field isGrouped>
+                <Control>
+                  <Button type="submit" isColor="primary">
+                    Envia
+                  </Button>
+                </Control>
+              </Field>
+            </form>
+          </Column>
+          <Column isSize={4} isOffset={1}>
+            <Content>
+              <p className="title is-size-4">
+                Pots omplir el formulari o si ho prefereixes escriure'ns
+                directament{' '}
+                <a href="mailto:hola@okstudio.tech">hola@okstudio.tech</a>
+              </p>
+            </Content>
+          </Column>
+          <Column isOffset={1} className="privacyContainer">
+            <div className="is-size-5">
+              Informació sobre la política de privacitat
+            </div>
+            <p className="privacyInfo">
+              Responsable: Ok! Studio - Oriol Castro Arnau.{'\n'} Finalitat:
+              Gestionar l'enviament d'informació relacionada amb la petició.{
+                '\n'
+              }
+              Legitimació: Consentiment de la persona interessada. Podrà exercir
+              els seus drets d'accés, rectificació, limitació o suprimir les
+              dades a hola@okstudio.tech{'\n'}
+              Destinataris: Ok! Studio - Oriol Castro Arnau.
+            </p>
+          </Column>
+        </Columns>
+      </Container>
+    </Section>
   </Layout>
 )
 
