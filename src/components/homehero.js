c02f7b0c686ce8a1e0d083670d7363268bf94a04
Uri <oriol.castroarnau@gmail.com>
2018-07-02 19:32:39 +0200

    Hero section in homepage

--- src/components/homehero.js
+++ src/components/homehero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import Typing from 'react-typing-animation'
 
 import { Title, Subtitle, Hero, HeroBody, Container } from 'bloomer'
 
@@ -9,8 +10,16 @@ const HomeHero = props => {
       <Hero isColor="primary" isSize="large" className="home-hero">
         <HeroBody>
           <Container>
-            <Title>{props.title}</Title>
-            <Subtitle>{props.subtitle}</Subtitle>
+            <Title isSize="2">{props.title}</Title>
+            <Subtitle>
+              <Typing speed={200} startDelay={2000} loop={true}>
+                <span>Webs modernes</span>
+                <Typing.Backspace count={13} />
+                <span>Aplicacions web</span>
+                <Typing.Backspace count={15} />
+                <span>Cloud computing</span>
+              </Typing>
+            </Subtitle>
           </Container>
         </HeroBody>
       </Hero>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-// import { Link } from 'gatsby'
 import LayoutHome from '../components/layoutHome'
 
 import HomeHero from '../components/homehero'
 
 const IndexPage = () => (
   <LayoutHome>
-    <HomeHero title="We are Ok! studio" />
+    <HomeHero title="I si la innovació tecnològica estigues a l'abast de tothom?" />
   </LayoutHome>
 )
 
