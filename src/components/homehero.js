ec1a2170418c89d7e013599fb8e7bf4319f7b948
Uri <oriol.castroarnau@gmail.com>
2018-07-04 14:41:19 +0200

    Update home hero background

--- src/components/homehero.js
+++ src/components/homehero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Typing from 'react-typing-animation'
 import { Link } from 'gatsby'
 import FAChevronRight from 'react-icons/lib/fa/chevron-right'
+import FAChevronDown from 'react-icons/lib/fa/chevron-down'
 import { Title, Subtitle, Hero, HeroBody, Container, Button } from 'bloomer'
 
 const HomeHero = props => {
@@ -12,11 +13,11 @@ const HomeHero = props => {
           <Container>
             <Title isSize="2">{props.title}</Title>
             <Subtitle isSize="3">
-              <Typing
+              {/* <Typing
                 speed={100}
                 cursorClassName="home-hero-cursor"
                 loop={true}
-              >
+                >
                 <span>Una web moderna</span>
                 <Typing.Backspace count={15} delay={500} speed={50} />
                 <Typing.Speed ms={100} />
@@ -30,21 +31,29 @@ const HomeHero = props => {
                 <Typing.Backspace count={23} delay={500} speed={50} />
                 <Typing.Speed ms={100} />
                 <span>Assesorament personalitzat</span>
-              </Typing>
+              </Typing> */}
+              <p className="invisible-space">.</p>
               <span className="invisible-space">.</span>
             </Subtitle>
             <Button isColor="white" isOutlined>
               <Link to="/serveis">
                 DESCOBREIX QUE PODEM FER PER TU
-                <FAChevronRight
+                {/* <FAChevronRight
                   height="1.25em"
                   width="1.25em"
                   className="my-icon-container"
-                />
+                /> */}
               </Link>
             </Button>
           </Container>
         </HeroBody>
+        <p className="has-text-centered add-bottom-margin has-text-primary">
+          <FAChevronDown
+            className="my-icon-container"
+            height="2em"
+            width="2em"
+          />
+        </p>
       </Hero>
     </div>
   )
