b20b2700477307a92449e05e5d1abf1abe062f1e
Uri <oriol.castroarnau@gmail.com>
2018-11-22 20:26:06 +0100

    Create page

---
+++ src/pages/lab.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Lab = data => {
+  return (
+    <Layout>
+      <h1>This is the LAB</h1>
+    </Layout>
+  )
+}
+
+export default Lab
